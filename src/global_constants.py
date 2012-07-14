f76751558258dbec4d35ec2366bfc00475cccbc4
andythenorth <mail@andythenorth.co.uk>
2012-07-14 20:31:34 +0100

    Codechange: remove redundant constant

--- src/global_constants.py
+++ src/global_constants.py
@@ -69,7 +69,6 @@ cargo_body_type_mappings = dict(
 )
 
 graphics_path = 'src/graphics/' # this is for nml, don't need to use python path module here
-BUY_MENU_X_OFFSET = 'NBSP'
 FIXED_RUN_COST = 500.0
 FUEL_RUN_COST = 10.0
 
