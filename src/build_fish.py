6972e76e884f88c17a6c1a2e1c47046aa948991e
andythenorth <mail@andythenorth.co.uk>
2012-07-21 19:59:13 +0100

    Change: cargo subtypes work across more cases of 'class is present'

--- src/build_fish.py
+++ src/build_fish.py
@@ -8,14 +8,15 @@ currentdir = os.curdir
 import sys
 sys.path.append(os.path.join('src')) # add to the module search path
 
-import codecs #used for writing files - more unicode friendly than standard open() module
+import codecs # used for writing files - more unicode friendly than standard open() module
 import math
+from string import Template # python builtin templater might be used in some utility cases
 
-from chameleon import PageTemplateLoader
+from chameleon import PageTemplateLoader # chameleon used in most template cases
 # setup the places we look for templates
-templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'), format='text')
-lang_templates = PageTemplateLoader(os.path.join(currentdir, 'lang'), format='text')
-docs_templates = PageTemplateLoader(os.path.join(currentdir,'docs'), format='text')
+templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'))
+lang_templates = PageTemplateLoader(os.path.join(currentdir, 'lang'))
+docs_templates = PageTemplateLoader(os.path.join(currentdir,'docs'))
 
 
 # the parser handles config file formats; provides a custom utility function for parsing to a list
@@ -132,7 +133,6 @@ class Ship(object):
 
     def get_buy_menu_string(self):
         # set buy menu text, with various variations
-        from string import Template
         if self.capacity_pax > 0 and self.capacity_freight > 0:
             buy_menu_template = Template(
                 "string(STR_BUY_MENU_TEXT, string(STR_${str_type_info}), string(STR_BUY_MENU_REFIT_CAPACITIES,${capacity_pax},${capacity_freight}), string(STR_${str_propulsion}))"
@@ -159,7 +159,8 @@ for i in config.sections():
 master_template = templates['fish.pynml']
 
 grf_nml = codecs.open(os.path.join('fish.nml'),'w','utf8')
-grf_nml.write(master_template(vehicles=vehicles, repo_vars=repo_vars))
+# an ugly hack here because chameleon html escapes the & characters needed by nml
+grf_nml.write('&'.join(master_template(vehicles=vehicles, repo_vars=repo_vars).split('$AMPERSAND')))
 grf_nml.close()
 
 
