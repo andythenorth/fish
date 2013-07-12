6b4bae8612e41b5026c8c9108310143a6cdb00be
andythenorth <mail@andythenorth.co.uk>
2013-07-12 08:24:13 +0100

    Codechange: add a utility file to mangle the order of ship properties, I'm bored of copy-pasting them

---
+++ src/mangle_ship_properties.py
@@ -0,0 +1,53 @@
+import os.path
+currentdir = os.curdir
+
+import sys
+sys.path.append(os.path.join('src')) # add to the module search path
+
+filenames = ['altamira_freighter.py',
+         'barletta_paddle_steamer.py',
+         'cape_spear_trawler.py',
+         'capo_sandalo_vehicle_ferry.py',
+         'castle_point_steamer.py',
+         'eddystone_tanker.py',
+         'endeavour_utility_catamaran.py',
+         'enoshima_catamaran_ferry.py',
+         'fastnet_paddle_steamer.py',
+         'feodosiya_hydrofoil.py',
+         'fish_island_trawler.py',
+         'frisco_bay_freighter.py',
+         'harbour_point_utility_vessel.py',
+         'hopetown_tanker.py',
+         'lindau_freight_barge.py',
+         'little_cumbrae_freighter.py',
+         'marstein_freighter.py',
+         'maspalomas_freighter.py',
+         'matsushima_hydrofoil.py',
+         'mount_blaze_catamaran_ferry.py',
+         'nieuwpoort_container_feeder.py',
+         'patos_island_vehicle_ferry.py',
+         'pine_island_log_tug.py',
+         'saint_marie_barge_tug.py',
+         'santorini_freighter.py',
+         'shark_island_livestock_ship.py',
+         'sunk_rock_ferry.py',
+         'thunder_bay_hovercraft.py',
+         'whitgift_freight_barge.py',
+         'yokohama_tanker.py']
+
+for filename in filenames:
+    file = open(os.path.join('src','ships',filename),'r')
+    content = file.readlines()
+    clean_content = []
+
+    for line in content:
+        if 'buy_cost' in line:
+            cut_line = line
+    content.remove(cut_line)
+    for line in content:
+        if 'fixed_run_cost_factor' in line:
+            line_to_insert_after = line
+    insert_position = content.index(line_to_insert_after)
+    content.insert(insert_position+1, cut_line)
+
+    print ''.join(content)
