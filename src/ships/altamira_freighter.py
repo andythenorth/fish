b05d8fc12e24d9a66a59edfce9496133fc4b3875
andythenorth <mail@andythenorth.co.uk>
2013-09-08 13:27:23 +0100

    Feature: new graphics for Altamira Freighter (Coxx)

--- src/ships/altamira_freighter.py
+++ src/ships/altamira_freighter.py
@@ -14,15 +14,23 @@ ship = GeneralCargoVessel(id = 'altamira_freighter',
             inland_capable = False,
             sea_capable = True,
             offsets = [[-14, -40], [-80, -24], [-66, -21], [-33, -25], [-14, -40], [-78, -26], [-66, -21], [-32, -23]],
-            buy_menu_width = 78,
+            buy_menu_width = 94,
             loading_speed = 20,
             intro_date = 1900,
-            buy_menu_bb_xy = [649, 21],
+            buy_menu_bb_xy = [646, 25],
             str_type_info = 'COASTER',
             vehicle_life = 40,
             gross_tonnage = 360,
             graphics_status = 'Work in Progress - Coxx',)
 
 ship.add_model_variant(intro_date=0,
-                       end_date=global_constants.max_game_date,
+                       end_date=1920,
                        spritesheet_suffix=0)
+
+ship.add_model_variant(intro_date=1910,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=1)
+
+ship.add_model_variant(intro_date=1960,
+                       end_date=global_constants.max_game_date,
+                       spritesheet_suffix=2)
