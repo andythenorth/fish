639a3fc758139a4acf840a0b9fd1083414befccc
andythenorth <mail@andythenorth.co.uk>
2014-08-09 18:44:57 +0100

    Change: turn off some ships in Euro roster

--- src/rosters/euro.py
+++ src/rosters/euro.py
@@ -1,48 +1,48 @@
 from roster import Roster
 
-from ships import altamira_freighter
-from ships import barletta_paddle_steamer
-from ships import cadiz_freighter
-from ships import cape_spear_trawler
-from ships import castle_point_steamer
+#from ships import altamira_freighter
+#from ships import barletta_paddle_steamer
+#from ships import cadiz_freighter
+#from ships import cape_spear_trawler
+#from ships import castle_point_steamer
 from ships import constance_freight_barge
 from ships import dieze_container_barge
-from ships import eddystone_tanker
-from ships import fish_island_trawler
-from ships import frisco_bay_freighter
+#from ships import eddystone_tanker
+#from ships import fish_island_trawler
+#from ships import frisco_bay_freighter
 from ships import geneva_freight_barge
-from ships import harbour_point_utility_vessel
-from ships import hopetown_tanker
-from ships import hovercraft_pax_large
-from ships import marstein_freighter
-from ships import maspalomas_freighter
-from ships import santorini_freighter
+#from ships import harbour_point_utility_vessel
+#from ships import hopetown_tanker
+#from ships import hovercraft_pax_large
+#from ships import marstein_freighter
+#from ships import maspalomas_freighter
+#from ships import santorini_freighter
 from ships import saint_marie_barge_tug
-from ships import shark_island_livestock_ship
-from ships import sunk_rock_ferry
-from ships import whitgift_freight_barge
-from ships import yokohama_tanker
+#from ships import shark_island_livestock_ship
+#from ships import sunk_rock_ferry
+#from ships import whitgift_freight_barge
+#from ships import yokohama_tanker
 
 roster = Roster(id = 'euro',
-                buy_menu_sort_order = ['harbour_point_utility_vessel',
-                                       'sunk_rock_ferry',
-                                       'barletta_paddle_steamer',
-                                       'castle_point_steamer',
-                                       'hovercraft_pax_large',
-                                       'fish_island_trawler',
-                                       'cape_spear_trawler',
-                                       'whitgift_freight_barge',
-                                       'marstein_freighter',
-                                       'altamira_freighter',
+                buy_menu_sort_order = [#'harbour_point_utility_vessel',
+                                       #'sunk_rock_ferry',
+                                       #'barletta_paddle_steamer',
+                                       #'castle_point_steamer',
+                                       #'hovercraft_pax_large',
+                                       #'fish_island_trawler',
+                                       #'cape_spear_trawler',
+                                       #'whitgift_freight_barge',
+                                       #'marstein_freighter',
+                                       #'altamira_freighter',
                                        'saint_marie_barge_tug',
                                        'constance_freight_barge',
                                        'geneva_freight_barge',
-                                       'cadiz_freighter',
-                                       'frisco_bay_freighter',
-                                       'santorini_freighter',
-                                       'maspalomas_freighter',
-                                       'eddystone_tanker',
-                                       'hopetown_tanker',
-                                       'yokohama_tanker',
-                                       'shark_island_livestock_ship',
+                                       #'cadiz_freighter',
+                                       #'frisco_bay_freighter',
+                                       #'santorini_freighter',
+                                       #'maspalomas_freighter',
+                                       #'eddystone_tanker',
+                                       #'hopetown_tanker',
+                                       #'yokohama_tanker',
+                                       #'shark_island_livestock_ship',
                                        'dieze_container_barge'])
