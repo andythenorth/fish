44febd669fdb4024822263ff9cec01b2bf42191d
andythenorth <mail@andythenorth.co.uk>
2014-08-17 18:28:57 +0100

    Change: graphics for Danube Large Ferry

--- src/fish.py
+++ src/fish.py
@@ -32,7 +32,7 @@ from rosters import registered_rosters
 
 # changing the order of rosters breaks savegames (parameter values change), don't do it.
 from rosters import brit
-#from rosters import euro
+from rosters import euro
 
 def get_ships_in_buy_menu_order(show_warnings=False):
     sorted_ships = []
