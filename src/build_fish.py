7ff8f7ef65ebf55e1bf3195319981241d166206b
andythenorth <mail@andythenorth.co.uk>
2015-02-19 13:38:48 +0000

    Codechange: instead of relying on magic, call main() explicitly for render_docs, render_lang, render_nml

--- src/build_fish.py
+++ src/build_fish.py
@@ -13,11 +13,14 @@ sys.path.append(os.path.join('src')) # add to the module search path
 
 # render the nml file
 import render_nml
+render_nml.main()
 
 # render the lang files
 import render_lang
+render_lang.main()
 
 # render the docs
 import render_docs
+render_docs.main()
 
 print((time() - start))
--- src/render_nml.py
+++ src/render_nml.py
@@ -8,36 +8,42 @@ sys.path.append(os.path.join('src')) # add to the module search path
 
 import codecs # used for writing files - more unicode friendly than standard open() module
 
-from chameleon import PageTemplateLoader # chameleon used in most template cases
-# setup the places we look for templates
-templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'))
-
 import fish
 import utils
 import global_constants
 
+from rosters import registered_rosters
+
 # get args passed by makefile
 repo_vars = utils.get_repo_vars(sys)
 
-ships = fish.get_ships_in_buy_menu_order(show_warnings=True)
+from chameleon import PageTemplateLoader # chameleon used in most template cases
+# setup the places we look for templates
+templates = PageTemplateLoader(os.path.join(currentdir, 'src', 'templates'))
 
-from rosters import registered_rosters
 
-grf_nml = codecs.open(os.path.join('fish.nml'),'w','utf8')
-header_items = ['header', 'cargo_table', 'disable_default_ships']
-for header_item in header_items:
-    template = templates[header_item + '.pynml']
-    templated_nml = utils.unescape_chameleon_output(template(ships=ships, global_constants=global_constants,
-                                                    registered_rosters=registered_rosters, utils=utils,
-                                                    sys=sys, repo_vars=repo_vars))
-    # append the results of templating
-    grf_nml.write(templated_nml)
+def main():
+    ships = fish.get_ships_in_buy_menu_order(show_warnings=True)
 
-for ship in set(ships):
-    templated_nml = ship.render()
-    # an ugly hack here because chameleon html escapes some characters
-    templated_nml = '>'.join(templated_nml.split('&gt;'))
-    templated_nml = '&'.join(templated_nml.split('&amp;'))
-    grf_nml.write(templated_nml)
 
-grf_nml.close()
+    grf_nml = codecs.open(os.path.join('fish.nml'),'w','utf8')
+    header_items = ['header', 'cargo_table', 'disable_default_ships']
+    for header_item in header_items:
+        template = templates[header_item + '.pynml']
+        templated_nml = utils.unescape_chameleon_output(template(ships=ships, global_constants=global_constants,
+                                                        registered_rosters=registered_rosters, utils=utils,
+                                                        sys=sys, repo_vars=repo_vars))
+        # append the results of templating
+        grf_nml.write(templated_nml)
+
+    for ship in set(ships):
+        templated_nml = ship.render()
+        # an ugly hack here because chameleon html escapes some characters
+        templated_nml = '>'.join(templated_nml.split('&gt;'))
+        templated_nml = '&'.join(templated_nml.split('&amp;'))
+        grf_nml.write(templated_nml)
+
+    grf_nml.close()
+
+if __name__ == '__main__':
+    main()
